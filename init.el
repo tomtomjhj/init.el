1d2f78a83cb5ee489a1e2e18a86cd8aaf20f8aaf
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2019-08-09 15:56:07 +0900

    evil

--- init.el
+++ init.el
@@ -1,1 +1,10 @@
-;; yes
+; evil.
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+(modify-syntax-entry ?_ "w") ; `_` isn't word char in emacs
+; - undo-tree
+; - PG
+; - tabbar-mode
+; - things from .spacemacs
+;   - undo-tree mapping of coq
