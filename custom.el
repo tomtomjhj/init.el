75d522f549e19e61029da792029437b1be1a1344
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2019-08-09 16:25:45 +0900

    zenburn, font, scale map

---
+++ custom.el
@@ -0,0 +1,15 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("f5376334e7bcbc22a27a07f51103012321adc7c22397d87d65927f9a7b4a4cf8" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 109 :width normal)))))
--- init.el
+++ init.el
@@ -1,8 +1,56 @@
+; custom-set-variables, -face things
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+; zenburn
+(add-to-list 'custom-theme-load-path "~/.emacs.d/zenburn")
+(setq zenburn-override-colors-alist
+        '(("zenburn-fg+1"     . "#FFFFEF")
+          ("zenburn-fg"       . "#eaeae0")
+          ("zenburn-fg-1"     . "#757565")
+          ("zenburn-bg-2"     . "#000000")
+          ("zenburn-bg-1"     . "#0f0f0f")
+          ("zenburn-bg-05"    . "#1f1f1f")
+          ("zenburn-bg"       . "#242424")
+          ("zenburn-bg+05"    . "#2f2f2f")
+          ("zenburn-bg+1"     . "#3F3F3F")
+          ("zenburn-bg+2"     . "#4F4F4F")
+          ("zenburn-bg+3"     . "#5F5F5F")
+          ))
+(load-theme 'zenburn)
+
+
+
 ; evil.
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
 (modify-syntax-entry ?_ "w") ; `_` isn't word char in emacs
+(require 'cl) ; lexcial-let
+(lexical-let*
+  ((default (face-attribute 'default :height))
+   (size default))
+
+  (defun global-scale-default ()
+    (interactive)
+    (setq size default)
+    (global-scale-internal size))
+
+  (defun global-scale-up ()
+    (interactive)
+    (global-scale-internal (incf size 10)))
+
+  (defun global-scale-down ()
+    (interactive)
+    (global-scale-internal (decf size 10)))
+
+  (defun global-scale-internal (arg)
+    (set-face-attribute 'default (selected-frame) :height arg)))
+(define-key evil-motion-state-map (kbd "C-=") 'global-scale-up)
+(define-key evil-motion-state-map (kbd "C-+") 'global-scale-up)
+(define-key evil-motion-state-map (kbd "C--") 'global-scale-down)
+(define-key evil-motion-state-map (kbd "C-0") 'global-scale-default)
+
 ; - undo-tree
 ; - PG
 ; - tabbar-mode
