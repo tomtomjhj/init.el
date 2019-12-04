1edcd711ededa0bd8ce5796f4e8d718b8e95e7de
Jaehwang Jerry Jung <tomtomjhj@gmail.com>
2019-12-04 14:50:08 +0900

    Disable auto-save. Reorg.

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
-; gc
+; vim: set foldmethod=marker foldlevel=0 nomodeline:
+; gc {{{
 (setq gc-cons-threshold 402653184
       gc-cons-percentage 0.6)
 
@@ -15,13 +16,12 @@
 
 (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
 (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
+; }}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; custom-set-variables, -face things
+; init {{{
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-; requires
 (require 'cl) ; lexcial-let
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -32,9 +32,9 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 (require 'use-package)
+; }}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; themes
+; themes {{{
 ; NOTE: hardcoded colors for region(block) and lazy-highlight(search)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/submodules/zenburn")
 (setq zenburn-override-colors-alist
@@ -51,20 +51,18 @@
 (load-theme 'zenburn)
 ; TODO: region should override lazy-highlight
 
-; TODO: it's not rainbow enough
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'coq-response-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'coq-goals-mode-hook 'rainbow-delimiters-mode)
+; }}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; evil.
+; evil {{{
 ; https://www.emacswiki.org/emacs/Evil#toc6
-
+; settings, loading, hooks {{{
 (setq scroll-preserve-screen-position t
     scroll-margin 3 ; vim's `scrolloff`
     scroll-conservatively 101)
-
 (setq evil-want-C-i-jump t)
 (setq evil-want-C-u-scroll t)
 (setq evil-want-integration t)
@@ -75,7 +73,6 @@
 ; https://github.com/syl20bnr/spacemacs/issues/8853
 (setq evil-want-abbrev-expand-on-insert-exit nil)
 
-
 (add-to-list 'load-path "~/.emacs.d/submodules/evil")
 (require 'evil)
 (evil-mode 1)
@@ -83,9 +80,10 @@
 ; `_` isn't word char in emacs
 (add-hook 'after-change-major-mode-hook
           (lambda () (modify-syntax-entry ?_ "w")))
-
 (evil-select-search-module 'evil-search-module 'evil-search)
+; }}}
 
+; basic {{{
 (lexical-let*
   ((default (face-attribute 'default :height))
    (size default))
@@ -130,6 +128,23 @@
 ; replace C-h with C-H
 (define-key global-map (kbd "C-S-H") 'help-command)
 
+; esc to quit everything.
+(defun minibuffer-keyboard-quit ()
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+    (setq deactivate-mark t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+; }}}
+
+; evil plugins {{{
 (add-to-list 'load-path "~/.emacs.d/submodules/evil-leader")
 (require 'evil-leader)
 (global-evil-leader-mode)
@@ -149,22 +164,6 @@
 (require 'evil-surround)
 (global-evil-surround-mode 1)
 
-; esc to quit everything.
-(defun minibuffer-keyboard-quit ()
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-    (setq deactivate-mark t)
-    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
-    (abort-recursive-edit)))
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-
-
 (add-to-list 'load-path "~/.emacs.d/submodules/evil-nerd-commenter")
 (require 'evil-nerd-commenter)
 (evil-leader/set-key
@@ -175,11 +174,6 @@
 (add-to-list 'load-path "~/.emacs.d/submodules/evil-visualstar")
 (require 'evil-visualstar)
 (global-evil-visualstar-mode)
-
-(add-to-list 'load-path "~/.emacs.d/submodules/evil-collection")
-(require 'evil-collection)
-(evil-collection-init)
-
 (defun my/star-keep-position ()
   (interactive)
   (evil-ex-search-word-forward)
@@ -189,7 +183,6 @@
   (when (region-active-p)
     (evil-visualstar/begin-search (region-beginning) (region-end) t)
     (evil-ex-search-previous)))
-
 (evil-global-set-key 'normal (kbd "*") 'my/star-keep-position)
 (evil-define-key 'visual evil-visualstar-mode-map (kbd "*") 'my/visualstar-keep-position)
 
@@ -201,9 +194,28 @@
 (evil-snipe-mode +1)
 (evil-snipe-override-mode +1)
 
+; NOTE: DO NOT UPDATE THIS SUBMODULE. Use 0.0.2.
+(add-to-list 'load-path "~/.emacs.d/submodules/evil-collection")
+(require 'evil-collection)
+(evil-collection-init)
+; }}}
+; }}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; undo
+; Neotree {{{
+(setq neo-hidden-regexp-list '("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "\\.o$" "\\.vo$" "\\.v\\.d$" "\\.glob$"))
+(setq neo-window-fixed-size nil)
+(setq neo-window-width 32)
+(setq neo-smart-open t)
+(setq neo-theme 'nerd)
+
+(add-to-list 'load-path "~/.emacs.d/submodules/emacs-neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(evil-leader/set-key
+  "n n" 'neotree-toggle)
+; }}}
+
+; undo {{{
 (setq undo-tree-auto-save-history t)
 (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
 (add-to-list 'load-path "~/.emacs.d/submodules/evil/lib")
@@ -213,13 +225,12 @@
 (define-key evil-normal-state-map (kbd "C-r") 'undo-tree-redo)
 ; TODO: undo-tree-undo'ing in proved(?) part may break something.
 ; https://github.com/ProofGeneral/PG/issues/430
-
 ; https://www.reddit.com/r/emacs/comments/6yzwic/how_emacs_undo_works/
 ; http://ergoemacs.org/emacs/emacs_undo_cult_problem.html
 ; what does undo-tree.el do in evil???
+;}}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; - PG
+; coq {{{
 (setq proof-splash-enable nil)
 (add-hook 'coq-mode-hook #'company-coq-mode)
 ; TODO: more keybindings from company-coq
@@ -253,26 +264,9 @@
   '(proof-eager-annotation-face ((t (:background "medium blue"))))
   '(proof-error-face ((t (:background "dark red"))))
   '(proof-warning-face ((t (:background "indianred3")))))
+; }}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Neotree
-(setq neo-hidden-regexp-list '("^\\." "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "\\.o$" "\\.vo$" "\\.v\\.d$" "\\.glob$"))
-(setq neo-window-fixed-size nil)
-(setq neo-window-width 32)
-(setq neo-smart-open t)
-(setq neo-theme 'nerd)
-
-(add-to-list 'load-path "~/.emacs.d/submodules/emacs-neotree")
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-(evil-leader/set-key
-  "n n" 'neotree-toggle)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; markdown https://leanpub.com/markdown-mode/read
-; TODO setup for pdf output
-; TODO html output is broken w/ quotes
-
+; markdown https://leanpub.com/markdown-mode/read {{{
 (use-package markdown-mode
   :ensure t
   :mode ("\\.md\\'" . gfm-mode)
@@ -284,8 +278,9 @@
          " --standalone --mathjax --ascii --highlight-style=kate"))
   (setq markdown-enable-math t)
   (setq markdown-use-pandoc-style-yaml-metadata t))
+; }}}
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; etc etc {{{
 (save-place-mode 1) ; cursor position
 (global-linum-mode 1)
 (setq column-number-mode t)
@@ -298,15 +293,33 @@
 (setq mouse-wheel-scroll-amount '(3))
 (setq mouse-wheel-progressive-speed nil)
 (setq ring-bell-function 'ignore)
-(setq make-backup-files nil)
+
+; http://ergoemacs.org/emacs/emacs_auto_save.html
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
+
+; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
+(defun my-backup-file-name (fpath)
+  (let* (
+        (backupRootDir "~/.emacs.d/backup/")
+        (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath ))
+        (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
+        )
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath
+  )
+)
+(setq make-backup-file-name-function 'my-backup-file-name)
+; }}}
 
 ; TODO: use-package for language-specific stuff? `:command` looks good
 ; examples: https://github.com/SkySkimmer/.emacs.d
 ; https://www.emacswiki.org/emacs/ELPA#toc5
-; TODO: remove submodules except zenburn
-; TODO: unicode input
+; TODO: remove submodules except zenburn?
 ; TODO: tabbar, CtrlP/fzf-like things, git gutter, MRU
 ; https://www.emacswiki.org/emacs/RecentFiles
-; TODO: general completion
-; TODO: '#file', 'file~', ....
-; https://stackoverflow.com/questions/12031830/what-are-file-and-file-and-how-can-i-get-rid-of-them
+; - https://github.com/bling/fzf.el
+; - helm?
+; https://www.emacswiki.org/emacs/RecentFiles
+; TODO: general keyword completion
+; - https://www.emacswiki.org/emacs/TabCompletion
